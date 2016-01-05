d526931cdd7c891ebe1ca04e6d004b098c9f60d5
liaklein <lklein13@u.rochester.edu>
2016-01-05 16:05:24 -0500

    almost done with graphics

--- frac.py
+++ frac.py
@@ -15,21 +15,42 @@ class Point:
 	
 	def __str__(self):
 		return "(" + str(self.x) + "," + str(self.y) + ")"
+
+def draw_single_line(line,turt):
+	#draws one line
+	turt.up()
+	turt.setpos(line.start.x, line.start.y)
+	turt.down()
+	turt.setpos(line.end.x, line.end.y)
 	
+def draw_lines(line_arr):
+	turt = turtle.Turtle()
+	turtle.shape("blank")
+	#draws each line in the line_arr
+	for line in line_arr:
+		draw_single_line(line,turt)
+	turtle.done()
+
 def iteration(line_arr):
 	#does the next iteration of the fractal
 	print "iteration"
 
 def main():
-	print "hello"
+	turtle.shape("blank")
 	#number of times you want the image to iterate
 	it = 5
+	num = 100
 	p1 = Point(0,0)
-	print p1
-	p2 = Point(0,1)
-	print p2
+	p2 = Point(0,num)
+	p3 = Point(num,num)
+	p4 = Point(num,0)
 	l1 = Line(p1,p2)
-	print l1	
+	l2 = Line(p2,p3)
+	l3 = Line(p3,p4)
+	l4 = Line(p4,p1)
+	line_arr = [l1,l2,l3,l4]
+	draw_lines(line_arr)
+
 
 if __name__ == "__main__":
 	main()
